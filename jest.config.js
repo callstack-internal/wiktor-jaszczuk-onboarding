3105ef1646def2a5d40d817d0f1fdb38e319d9fd
Wiktor Jaszczuk <wiktor.jaszczuk@callstack.com>
2025-03-04 14:15:59 +0100

    issue_11 add custom path support to jest, TS and babel

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
+const {pathsToModuleNameMapper} = require('ts-jest');
+const {compilerOptions} = require('./tsconfig.json');
+
 module.exports = {
   preset: 'react-native',
+  roots: ['<rootDir>'],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
 };
