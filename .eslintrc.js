97e01c2a869f8f905209f001b04da6a320557a5c
Wiktor Jaszczuk <wiktor.jaszczuk@callstack.com>
2025-03-05 09:07:41 +0100

    issue_2 add jest global symbol support as an override to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,22 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  overrides: [
+    {
+      files: [
+        'jest.setup.js',
+        '**/*.spec.js',
+        '**/*.spec.jsx',
+        '**/*.spec.ts',
+        '**/*.spec.tsx',
+        '**/*.test.js',
+        '**/*.test.jsx',
+        '**/*.test.ts',
+        '**/*.test.tsx',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
