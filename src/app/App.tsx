eaf664c011174c6d9f7ca772db40ee98f8804eef
Wiktor Jaszczuk <wiktor.jaszczuk@callstack.com>
2025-03-05 15:02:03 +0100

    15-fetch-data-from-open-weather-map install react-native-safe-area-context together with mock declarations and apply it in App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,21 @@
 import {useCloseBootsplashOnMount} from '@hooks/useCloseBootsplashOnMount';
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
 import Config from 'react-native-config';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 
 export function App() {
   useCloseBootsplashOnMount();
 
-  return <Text testID="OW_KEY">{Config.OPEN_WEATHER_API_KEY}</Text>;
+  return (
+    <SafeAreaProvider style={styles.container}>
+      <SafeAreaView>
+        <Text testID="OW_KEY">{Config.OPEN_WEATHER_API_KEY}</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {flex: 1},
+});
