e4bf2f5708899ef4164884409ac776497b9b14dc
Wiktor Jaszczuk <wiktor.jaszczuk@callstack.com>
2025-03-05 14:45:07 +0100

    15-fetch-data-from-open-weather-map clear mocks before every test in jest

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ const {compilerOptions} = require('./tsconfig.json');
 module.exports = {
   preset: 'react-native',
   roots: ['<rootDir>'],
+  clearMocks: true,
   modulePaths: [compilerOptions.baseUrl],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
 };
