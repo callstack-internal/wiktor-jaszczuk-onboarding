0d89fc45f59a44c8e5f9e78addd6629d5b0b3523
Wiktor Jaszczuk <wiktor.jaszczuk@callstack.com>
2025-03-05 14:45:56 +0100

    15-fetch-data-from-open-weather-map display current open weather api key in the app + tests

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
+import {App} from '@app/App';
 import {AppRegistry} from 'react-native';
-import App from '@app/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,13 +1,27 @@
-/**
- * @format
- */
-
 import React from 'react';
-import ReactTestRenderer from 'react-test-renderer';
-import App from '@app/App';
+import {render, screen} from '@testing-library/react-native';
+import {App} from '@app/App';
+import ReactNativeBootsplash from 'react-native-bootsplash';
 
-test('renders correctly', async () => {
-  await ReactTestRenderer.act(() => {
-    ReactTestRenderer.create(<App />);
+const MOCKED_OW_KEY = 'THIS_IS_SOME_MOCKED_OW_KEY';
+
+describe('App', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  it('displays mocked open weather key key on the screen', () => {
+    expect(screen.getByText(MOCKED_OW_KEY)).toBeOnTheScreen();
+  });
+
+  it('has element with testID OW_KEY contains mocked open weather key', () => {
+    const textElement = screen.getByTestId('OW_KEY');
+
+    expect(textElement).toBeOnTheScreen();
+    expect(textElement).toHaveTextContent(MOCKED_OW_KEY);
+  });
+
+  it('it hides bootsplash image on app start', async () => {
+    expect(ReactNativeBootsplash.hide).toHaveBeenCalledTimes(1);
   });
 });
