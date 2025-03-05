14f3515d249d2b0e75cb483ca04b0197d52cebe4
Wiktor Jaszczuk <wiktor.jaszczuk@callstack.com>
2025-03-05 14:42:50 +0100

    15-fetch-data-from-open-weather-map install react-native-config and @testing-library/react-native
    declare module for react-native-config for type safety
    add mock data for react-native-config. In __mock__ folder

---
+++ src/types/react-native-config.d.ts
@@ -0,0 +1,8 @@
+declare module 'react-native-config' {
+  export interface NativeConfig {
+    OPEN_WEATHER_API_KEY?: string;
+  }
+
+  export const Config: NativeConfig;
+  export default Config;
+}
