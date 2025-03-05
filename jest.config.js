1af6df8cc4596b34e47d22c79e2ee39ecc9ef83b
Wiktor Jaszczuk <wiktor.jaszczuk@callstack.com>
2025-03-05 15:02:03 +0100

    15-fetch-data-from-open-weather-map install react-native-safe-area-context together with mock declarations and apply it in App.tsx

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,8 @@ module.exports = {
   clearMocks: true,
   modulePaths: [compilerOptions.baseUrl],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-native-safe-area-context)/)',
+  ],
 };
