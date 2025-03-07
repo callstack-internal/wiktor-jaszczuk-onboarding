77b496e263b5dfba695d7f21104471b780afe2dc
Wiktor Jaszczuk <wiktor.jaszczuk@callstack.com>
2025-03-07 15:02:59 +0100

    15-fetch-data-from-open-weather-map import react-native-community/netifo

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,7 @@
 import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock';
-import {server} from './src/msw/server';
+import mockRNCNetInfo from '@react-native-community/netinfo/jest/netinfo-mock.js';
 
+jest.mock('@react-native-community/netinfo', () => mockRNCNetInfo);
 jest.mock('react-native-safe-area-context', () => mockSafeAreaContext);
 
 beforeAll(() => server.listen());
