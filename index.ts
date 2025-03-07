91c69a1f8d3f6d6e2c09574f3bbc04a066c7762f
Wiktor Jaszczuk <wiktor.jaszczuk@callstack.com>
2025-03-07 15:04:44 +0100

    15-fetch-data-from-open-weather-map fix msw

--- index.ts
+++ index.ts
@@ -1,16 +1,6 @@
-import {App} from '@app/App';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import {Config} from 'react-native-config';
+import {App} from './src/components/App';
 
-async function enableMocking() {
-  if (Config.MOCK_OPEN_WEATHER_API === '1') {
-    await import('./src/msw/msw.polyfills');
-    const {server} = await import('./src/msw/server');
-    server.listen();
-  }
-}
+AppRegistry.registerComponent(appName, () => App);
 
-enableMocking().then(() => {
-  AppRegistry.registerComponent(appName, () => App);
-});
---
+++ src/msw/index.ts
@@ -0,0 +1,2 @@
+export {server} from './server';
+export {MSWProvider} from './MSWProvider';
