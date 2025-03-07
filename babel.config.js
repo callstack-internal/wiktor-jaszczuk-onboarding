884d7f38b5a7ed8014fbe2a49280b55bc4ddb101
Wiktor Jaszczuk <wiktor.jaszczuk@callstack.com>
2025-03-07 14:56:19 +0100

    15-fetch-data-from-open-weather-map resign from path aliases

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,3 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-        alias: {
-          '@app': './src/app',
-          '@components': './src/components',
-          '@hooks': './src/hooks',
-          '@screens': './src/screens',
-          '@services': './src/services',
-        },
-      },
-    ],
-  ],
+  presets: ['module:@react-native/babel-preset']
 };
