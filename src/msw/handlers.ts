f0d30624c4f6678ff4ab03241c2394be3920ce3a
Wiktor Jaszczuk <wiktor.jaszczuk@callstack.com>
2025-03-07 16:36:26 +0100

    15-fetch-data-from-open-weather-map fix http.get is undefined

--- src/msw/handlers.ts
+++ src/msw/handlers.ts
@@ -9,12 +9,14 @@ import {
 import geoEndpointResponse from './responses/geo_response_wro.json';
 import weatherEndpointResponse from './responses/weather_response_wro.json';
 
-export const handlers = [
-  http.get(OPEN_WEATHER_API_URL + OPEN_WEATHER_GEO_ENDPOINT_URI, () =>
-    HttpResponse.json(geoEndpointResponse),
-  ),
-  http.get(
-    OPEN_WEATHER_API_URL + OPEN_WEATHER_CURRENT_WEATHER_ENDPOINT_URI,
-    () => HttpResponse.json(weatherEndpointResponse),
-  ),
-];
+const geoHandler = http.get(
+  OPEN_WEATHER_API_URL + OPEN_WEATHER_GEO_ENDPOINT_URI,
+  () => HttpResponse.json(geoEndpointResponse),
+);
+
+const weatherHandler = http.get(
+  OPEN_WEATHER_API_URL + OPEN_WEATHER_CURRENT_WEATHER_ENDPOINT_URI,
+  () => HttpResponse.json(weatherEndpointResponse),
+);
+
+export const handlers = [geoHandler, weatherHandler];
