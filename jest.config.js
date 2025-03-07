d687324bbe84274d509e3575fa524ed52c9a3e8d
Wiktor Jaszczuk <wiktor.jaszczuk@callstack.com>
2025-03-07 14:56:19 +0100

    15-fetch-data-from-open-weather-map resign from path aliases

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,6 @@
-const {pathsToModuleNameMapper} = require('ts-jest');
-const {compilerOptions} = require('./tsconfig.json');
-
 module.exports = {
   preset: 'react-native',
-  roots: ['<rootDir>'],
   clearMocks: true,
-  modulePaths: [compilerOptions.baseUrl],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-native-safe-area-context)/)',
