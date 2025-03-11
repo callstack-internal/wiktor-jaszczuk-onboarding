95b75ca8ea3df408845d2d882ca940295bdac1ba
Wiktor Jaszczuk <wiktor.jaszczuk@callstack.com>
2025-03-11 11:08:56 +0100

    18-add-i18n add profile feature stub

--- src/components/App/NavigationContainer.tsx
+++ src/components/App/NavigationContainer.tsx
@@ -1,29 +1,49 @@
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import {
   createStaticNavigation,
   type StaticParamList,
 } from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {screens} from '../../features/weather';
+import {
+  createNativeStackNavigator,
+  type NativeStackNavigationProp,
+} from '@react-navigation/native-stack';
+import {screens as weatherScreens} from '../../features/weather';
+import {screens as profileScreens} from '../../features/profile';
 
-const RootStack = createNativeStackNavigator({
+const WeatherStack = createNativeStackNavigator({
   initialRouteName: 'WeatherLocations',
   screens: {
     WeatherLocations: {
-      screen: screens.WeatherLocations,
+      screen: weatherScreens.WeatherLocations,
       options: {
         title: 'Locations',
       },
     },
-    WeatherLocationDetails: screens.WeatherLocationDetails,
+    WeatherLocationDetails: weatherScreens.WeatherLocationDetails,
   },
 });
 
-export const NavigationContainer = createStaticNavigation(RootStack);
+const RootNavigator = createDrawerNavigator({
+  screens: {
+    Home: WeatherStack,
+    Profile: profileScreens.ProfileScreen,
+  },
+});
 
-type RootStackParamList = StaticParamList<typeof RootStack>;
+export const NavigationContainer = createStaticNavigation(RootNavigator);
 
 declare global {
   namespace ReactNavigation {
-    interface RootParamList extends RootStackParamList {}
+    interface RootParamList extends StaticParamList<typeof RootNavigator> {}
+
+    interface WeatherStackParamList
+      extends StaticParamList<typeof WeatherStack> {}
+
+    interface WeatherNavigationProp<
+      SCREEN extends keyof ReactNavigation.WeatherStackParamList,
+    > extends NativeStackNavigationProp<
+        ReactNavigation.WeatherStackParamList,
+        SCREEN
+      > {}
   }
 }
