280df6d3902ee110ca55b98322ffd085d3d4f4dc
Wiktor Jaszczuk <wiktor.jaszczuk@callstack.com>
2025-03-11 11:32:51 +0100

    18-add-i18n fix wrong reanimated jest test configuration

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
   clearMocks: true,
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-native-safe-area-context|@react-navigation)/)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-native-safe-area-context|@react-navigation|react-native-reanimated)/)',
   ],
 };
--- jest.setup.js
+++ jest.setup.js
@@ -1,8 +1,10 @@
+import 'react-native-gesture-handler/jestSetup';
 import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock';
 import mockRNCNetInfo from '@react-native-community/netinfo/jest/netinfo-mock.js';
 import {server} from './src/msw';
+import {setUpTests} from 'react-native-reanimated';
 
-import 'react-native-gesture-handler/jestSetup';
+setUpTests();
 
 jest.mock('@react-native-community/netinfo', () => mockRNCNetInfo);
 jest.mock('react-native-safe-area-context', () => mockSafeAreaContext);
