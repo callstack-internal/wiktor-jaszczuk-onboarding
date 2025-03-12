f612901f759e18b0ed2c77e549ab1413ac363e3a
Wiktor Jaszczuk <wiktor.jaszczuk@callstack.com>
2025-03-12 12:50:51 +0100

    18-add-i18n change language

--- src/features/language/index.ts
+++ src/features/language/index.ts
@@ -1,6 +1,12 @@
+import {supportedLngs} from './i18next/config';
+
 export {
-  supportedLngs as SUPPORTED_LANGUAGES,
+  supportedLngs,
   getI18nInstance,
+  changeLanguage,
+  getCurrentLanguage,
 } from './i18next/config';
 export {useTranslation} from 'react-i18next';
 export {TranslationProvider} from './components/TranslationProvider';
+
+export type SupportedLanguage = (typeof supportedLngs)[number];
